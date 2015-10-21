65f6138bc8239220842a928b503e6b1e337f0837
lasko2112 <coll56667@yahoo.co.uk>
2015-10-21 13:35:47 +0100

    [Added] Generals bunker screenplay

--- MMOCoreORB/bin/scripts/mobile/custom_content/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_content/serverobjects.lua
@@ -3,6 +3,7 @@
 --Child Folders
 --Mustafar (som)
 includeFile("custom_content/ep3/serverobjects.lua")
+includeFile("custom_content/screenplay_mobs/serverobjects.lua")
 includeFile("custom_content/som/serverobjects.lua")
 
 
---
+++ MMOCoreORB/bin/scripts/screenplays/custom_content/screenplays.lua
@@ -0,0 +1,6 @@
+--Custom Content
+
+--includeFile("screenplays/new.lua")
+
+--Dungeons
+includeFile("custom_content/dungeon/generals_bunker.lua")
