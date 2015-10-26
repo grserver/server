f4b71d42c15b7e4cdc68415e3825b092847289f7
lasko2112 <coll56667@yahoo.co.uk>
2015-10-26 17:16:08 +0000

    [Added] Spawns

--- MMOCoreORB/bin/scripts/mobile/custom_content/serverobjects.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_content/serverobjects.lua
@@ -4,8 +4,10 @@
 --Mustafar (som)
 includeFile("custom_content/ep3/serverobjects.lua")
 includeFile("custom_content/ghomrassen/serverobjects.lua")
+includeFile("custom_content/lair/serverobjects.lua")
 includeFile("custom_content/screenplay_mobs/serverobjects.lua")
 includeFile("custom_content/som/serverobjects.lua")
+includeFile("custom_content/spawn/serverobjects.lua")
 
 
 
