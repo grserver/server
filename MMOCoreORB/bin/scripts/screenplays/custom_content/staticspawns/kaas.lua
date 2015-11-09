4f52a13356ef9416ddb4ae343343e4e43ede0cdf
terminator2014 <terminatorgithub@hotmail.com>
2015-11-09 12:43:46 +0000

    [Added] More buildings to kaas

--- MMOCoreORB/bin/scripts/screenplays/custom_content/staticspawns/kaas.lua
+++ MMOCoreORB/bin/scripts/screenplays/custom_content/staticspawns/kaas.lua
@@ -18,9 +18,10 @@ end
 function kaasScreenPlay:spawnSceneObjects()
 
 	--Buildings & Props
-	spawnSceneObject("kaas", "object/building/yavin/temple_exar_kun.iff", -2934.3, 180.0, 3590.5, 101, 0 )
-	spawnSceneObject("kaas", "object/building/corellia/skyscraper_corellia_style_04.iff", -6110.4, 80.0, 2747.1, -91, 0 )
-	spawnSceneObject("kaas", "object/building/corellia/skyscraper_corellia_style_03.iff", -6243.2, 80.0, 2739.7, 93, 0 )
+	spawnSceneObject("kaas", "object/building/yavin/temple_exar_kun.iff", -2926.9, 180.0, 3584.5, -65, 0 )
+	spawnSceneObject("kaas", "object/building/corellia/skyscraper_corellia_style_04.iff", -6089.2, 80.0, 2645.1, -2, 0 )
+	spawnSceneObject("kaas", "object/building/corellia/skyscraper_corellia_style_04.iff", -6243.2, 80.0, 2739.7, 93, 0 )
+	spawnSceneObject("kaas", "object/building/corellia/starport_corellia.iff", -6185.5, 80.0, 2984.5, -177, 0 )
 	
 end
 
