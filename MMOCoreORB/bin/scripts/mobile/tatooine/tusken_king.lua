4c770a02fe3c42fa975d46eef497494a263e68a6
terminator2014 <terminatorgithub@hotmail.com>
2016-01-04 12:39:42 +0000

    [Edited] Mobs and added screenplay

--- MMOCoreORB/bin/scripts/mobile/tatooine/tusken_king.lua
+++ MMOCoreORB/bin/scripts/mobile/tatooine/tusken_king.lua
@@ -2,7 +2,7 @@ tusken_king = Creature:new {
 	objectName = "@mob/creature_names:tusken_king",
 	socialGroup = "tusken_raider",
 	faction = "tusken_raider",
-	level = 100,
+	level = 125,
 	chanceHit = 1,
 	damageMin = 645,
 	damageMax = 1000,
@@ -10,7 +10,7 @@ tusken_king = Creature:new {
 	baseHAM = 24000,
 	baseHAMmax = 30000,
 	armor = 2,
-	resists = {45,35,5,80,-1,50,5,5,-1},
+	resists = {35,35,5,70,-1,35,5,5,-1},
 	meatType = "",
 	meatAmount = 0,
 	hideType = "",
@@ -29,17 +29,19 @@ tusken_king = Creature:new {
 	lootGroups = {
 		{
 			groups = {
-				{group = "junk", chance = 1500000},
-				{group = "tusken_common", chance = 3000000},
-				{group = "wearables_all", chance = 2000000},
-				{group = "bone_armor", chance = 750000},
-				{group = "chitin_armor", chance = 750000},
-				{group = "armor_attachments", chance = 500000},
-				{group = "clothing_attachments", chance = 500000},
-				{group = "color_crystals", chance = 500000},
-				{group = "crystals_quality", chance = 500000}
+				{group = "wearables_scarce", chance = 2500000},
+				{group = "holocron_dark", chance = 2500000},
+				{group = "color_crystals", chance = 2500000},
+				{group = "pearls_premium", chance = 2500000}
 			},
-			lootChance = 4500000
+			lootChance = 6000000
+			},
+			{
+			groups = {
+			{group = "armor_attachments", chance = 5000000},
+			{group = "clothing_attachments", chance = 5000000},
+			},
+			lootChance = 6000000
 		}
 	},
 	weapons = {"tusken_weapons"},
