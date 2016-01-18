df62cb7b83652f7b9e15a79040d1ae8c2f0f4048
terminator2014 <terminatorgithub@hotmail.com>
2016-01-18 09:05:53 +0000

    [Added] new skills to admin

--- MMOCoreORB/bin/scripts/staff/levels/admin.lua
+++ MMOCoreORB/bin/scripts/staff/levels/admin.lua
@@ -15,6 +15,7 @@ admin = {
 		"admin_player_management_01",
 		"admin_player_management_02",
 		"admin_player_management_03",
+		"admin_player_management_04",
 		"admin_quest_management_01",
 		"admin_server_admin_01",
 		"admin_server_admin_02",
