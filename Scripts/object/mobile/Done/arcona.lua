34b9f6197251e7c9e13306fd342b96b3a0074fbb
lasko2112 <coll56667@yahoo.co.uk>
2016-02-08 08:46:07 +0000

    [Added] Done folders, move complete

---
+++ Scripts/object/mobile/Done/arcona.lua
@@ -0,0 +1,3 @@
+object_mobile_arcona = object_mobile_shared_arcona:new {
+}
+ObjectTemplates:addTemplate(object_mobile_arcona, "object/mobile/arcona.iff")
--- Scripts/object/mobile/ados_jarkey.lua
+++
@@ -1,1 +0,0 @@
-object_mobile_del = object_mobile_shared_del:new {}ObjectTemplates:addTemplate(object_mobile_del, "object/mobile/del.iff")
